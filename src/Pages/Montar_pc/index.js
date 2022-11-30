6c11ed2ea6f364ef588f2a036d59f2bc968499ac
Hector Lopes <hector.lopes@anchieta.br>
2022-11-30 09:44:45 -0300

    feat:Change images with Js

--- src/Pages/Montar_pc/index.js
+++ src/Pages/Montar_pc/index.js
@@ -1,7 +1,34 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
 
 function change_image(){
 
-    var imagem = $("#imagem").val();
-    alert(imagem);
+    var imagem = $("#imagem").val(); console.log(imagem);
+  var processador =$("#processador").val();
+
+  switch (processador) {
+    case 'Pentiu':
+        img.src = '/assets/Processador/pentium7gen.png'
+      break;
+    case 'I3':
+        img.src = '/assets/Processador/i3.png'
+    break;   
+    case 'I5':
+        img.src = '/assets/Processador/i5.png'
+    break;
+    case 'I7':
+        img.src = '/assets/Processador/i7.png'
+    break;
+    case 'I9':
+        img.src = '/assets/Processador/i9.png'
+        break;
+    default: 
+      console.log(`Sorry, we are out of ${expr}.`);
+
+
 }
+
+
+
+}
+
+
