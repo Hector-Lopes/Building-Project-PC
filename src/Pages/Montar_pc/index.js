b7bcaca328b3a17d1d6ddd1b05d1b16648f1c604
Hector Lopes <hector.lopes@anchieta.br>
2022-11-30 11:49:23 -0300

    perf:Modficantion in the mode of change the images

--- src/Pages/Montar_pc/index.js
+++ src/Pages/Montar_pc/index.js
@@ -6,29 +6,47 @@ function change_image(){
   var processador =$("#processador").val();
 
   switch (processador) {
-    case 'Pentiu':
+    case '1':
         img.src = '/assets/Processador/pentium7gen.png'
       break;
-    case 'I3':
+    case '2':
         img.src = '/assets/Processador/i3.png'
     break;   
-    case 'I5':
+    case '3':
         img.src = '/assets/Processador/i5.png'
     break;
-    case 'I7':
+    case '4':
         img.src = '/assets/Processador/i7.png'
     break;
-    case 'I9':
+    case '5':
         img.src = '/assets/Processador/i9.png'
         break;
     default: 
       console.log(`Sorry, we are out of ${expr}.`);
 
+    } 
+
+}
+document.getElementById("enviar").onclick = function (){
+     $(".PROCESSADOR_DIV").hide();
+ }
+
+
+function add_mother(){
+   //Adiciona um listener no onClick
+$('#adicionarEmail').click( () => 
+//Pega o ListEmail adiciona um elemento
+$('#PROCESSADOR_DIV').append( 
+     //cria um elemento li e põe o texto como o valor do input
+     $('<form>').text( $('#PROCESSADOR_DIV_GUARDADO').val() ) 
+)
+);
+    
+    document.getElementById("PROCESSADOR_DIV").appendChild(li);
+    
 
 }
 
 
-
-}
 
 
