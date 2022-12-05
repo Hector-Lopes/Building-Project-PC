76800012379b8bf8a3912ffb6f114190f1a03f03
Hector Lopes <hector.lopes@anchieta.br>
2022-12-05 12:55:10 -0300

    att:chnage the array for object arryy

--- src/Pages/Montar_pc/index.js
+++ src/Pages/Montar_pc/index.js
@@ -15,27 +15,27 @@ function change_image(){
     break;   
     case 'I5':
         img.src = '/assets/Processador/i5.png'
-    
+    trocaformularios()
         break;
     case 'I7':
         img.src = '/assets/Processador/i7.png'
     break;
     case 'I9':
         img.src = '/assets/Processador/i9.png'
-        function testediv(){
-          const teste =$("#teste").val();
+//         function testediv(){
+//           const teste =$("#teste").val();
   
-          const a= document.createElement('a')
+//           const a= document.createElement('a')
 
-a.textContent='a';
+// a.textContent='a';
 
-  teste.append(a);
+//   teste.append(a);
 
-  document.getElementById("teste").textContent = 
-  document.getElementById("teste").innerHTML  
+//   document.getElementById("teste").textContent = 
+//   document.getElementById("teste").innerHTML  
 
 
-}
+// }
         break;
     default: 
       console.log(`Sorry, we are out of ${expr}.`);
@@ -49,30 +49,41 @@ a.textContent='a';
 //      $(".PROCESSADOR_DIV").hide();
 //  }
 
-var formularios = [
-  {     
-      nome : "Processador",
-      peca1 : "Pentium",
-      peca2 : "I3",
-      peca3 : "I5",
-      peca4 : "I7",
-      peca5 : "I9",
+
+
+
+function trocaformularios(Chooseform){
+//array objstos
+const vlores = [
+  {
+    nomePeca: "",
+    preco: "",
+    quantidade: ""
+  },
+  {
+    nomePeca: "",
+    preco: "",
+    quantidade: ""
+  },
+  {
+    nomePeca: "",
+    preco: "",
+    quantidade: ""
+  }
+]
+
+
   
-    }
- 
-];
-function trocaformularios(){
-  
-  var  Chooseform= formularios.map(function(item, indice){     
+  var  Chooseform= vlores.map(function(item,){     
     return ` <form >
     <label  for="processador">Escolha o Processador</label>
     <select onchange="change_image()" name="processador" id="processador">
       <optgroup label="Intel">
-        <option value= "${item.peca1}" >${item.peca1}</option>
-        <option value="${item.peca2}">${item.peca2}</option>
-        <option value="${item.peca3}">${item.peca3}</option>
-        <option value="${item.peca4}">${item.peca4}</option>
-        <option value="${item.peca5}">${item.peca5}</option>
+        <option value="${vlores[1].nomePeca}">${vlores[1].nomePeca}</option>
+        <option value="${vlores[2].nomePeca}">${vlores[2].nomePeca}</option>
+        <option value="${vlores[3].nomePeca}">${vlores[3].nomePeca}</option>
+        <option value="${vlores[4].nomePeca}">${vlores[4].nomePeca}</option>
+        <option value="${vlores[5].nomePeca}">${vlores[5].nomePeca}</option>
       </optgroup>
       <optgroup label="AMD">
         <option value="Ryzen3">Ryzen 3</option>
@@ -87,9 +98,9 @@ function trocaformularios(){
   
           });
   
-      document.querySelector("#teste2").innerHTML = Chooseform.join("");
+      document.querySelector("#teste2").innerHTML = Chooseform;
  
-
+return trocaformularios();
 
 }
 
