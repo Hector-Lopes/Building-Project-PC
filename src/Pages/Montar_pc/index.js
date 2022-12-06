529358e50217858b418e772267448174beb89b56
Hector Lopes <hector.lopes@anchieta.br>
2022-12-06 12:59:30 -0300

    feat:building forms with objects

--- src/Pages/Montar_pc/index.js
+++ src/Pages/Montar_pc/index.js
@@ -3,7 +3,7 @@
 function change_image(){
 
    
-    const processador =$("#processador").val();
+    const processador =$("#formu").val();
 
   switch (processador) {
     case 'Pentium':
@@ -15,27 +15,13 @@ function change_image(){
     break;   
     case 'I5':
         img.src = '/assets/Processador/i5.png'
-    trocaformularios()
+        trocaformularios()
         break;
     case 'I7':
         img.src = '/assets/Processador/i7.png'
     break;
     case 'I9':
         img.src = '/assets/Processador/i9.png'
-//         function testediv(){
-//           const teste =$("#teste").val();
-  
-//           const a= document.createElement('a')
-
-// a.textContent='a';
-
-//   teste.append(a);
-
-//   document.getElementById("teste").textContent = 
-//   document.getElementById("teste").innerHTML  
-
-
-// }
         break;
     default: 
       console.log(`Sorry, we are out of ${expr}.`);
@@ -48,62 +34,96 @@ function change_image(){
 // document.getElementById("enviar").onclick = function (){
 //      $(".PROCESSADOR_DIV").hide();
 //  }
-
-
-
-
-function trocaformularios(Chooseform){
-//array objstos
 const vlores = [
   {
-    nomePeca: "",
+    nomePeca: "Pentium",
     preco: "",
     quantidade: ""
   },
   {
-    nomePeca: "",
+    nomePeca: "I3",
     preco: "",
     quantidade: ""
   },
   {
-    nomePeca: "",
+    nomePeca: "I5",
     preco: "",
     quantidade: ""
-  }
+  },
+  {
+    nomePeca: "I7",
+    preco: "",
+    quantidade: ""
+  },
+  {
+    nomePeca: "I9",
+    preco: "",
+    quantidade: ""
+  },
+  {
+    nomePeca: "Ryzen 3",
+    preco: "",
+    quantidade: ""
+  },
+  {
+    nomePeca: "Ryzen 5",
+    preco: "",
+    quantidade: ""
+  },
+  {
+    nomePeca: "Ryzen 7",
+    preco: "",
+    quantidade: ""
+  },
+  {
+    nomePeca: "Ryzen 9",
+    preco: "",
+    quantidade: ""
+  },
+  
+
+
+
+
+
 ]
 
 
+
+function trocaformularios(){
+//array objstos
+
+
+
   
-  var  Chooseform= vlores.map(function(item,){     
+  var  Chooseform  = vlores.map(function(a,b) {     
     return ` <form >
     <label  for="processador">Escolha o Processador</label>
     <select onchange="change_image()" name="processador" id="processador">
       <optgroup label="Intel">
+        <option value="${vlores[0].nomePeca}">${vlores[0].nomePeca}</option>
         <option value="${vlores[1].nomePeca}">${vlores[1].nomePeca}</option>
         <option value="${vlores[2].nomePeca}">${vlores[2].nomePeca}</option>
         <option value="${vlores[3].nomePeca}">${vlores[3].nomePeca}</option>
         <option value="${vlores[4].nomePeca}">${vlores[4].nomePeca}</option>
-        <option value="${vlores[5].nomePeca}">${vlores[5].nomePeca}</option>
       </optgroup>
       <optgroup label="AMD">
-        <option value="Ryzen3">Ryzen 3</option>
-        <option value="Ryzen5">Ryzen 5</option>
-        <option value="Ryzen7">Ryzen 7</option>
-        <option value="Ryzen9">Ryzen 9</option>
+        <option value=${vlores[5].nomePeca}>${vlores[5].nomePeca}</option>
+        <option value=${vlores[6].nomePeca}>${vlores[6].nomePeca}</option>
+        <option value=${vlores[7].nomePeca}>${vlores[7].nomePeca}</option>
+        <option value=${vlores[8].nomePeca}>${vlores[8].nomePeca}</option>
   
       </optgroup>
     </select>`;
   
   
   
-          });
+  });
   
-      document.querySelector("#teste2").innerHTML = Chooseform;
- 
-return trocaformularios();
+  
+  
+  document.querySelector("#teste2").innerHTML =Chooseform.join("");
+
+console.log(Chooseform)
 
 }
-
-
-
-
