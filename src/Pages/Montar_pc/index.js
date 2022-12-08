c551b05bebca6808c8b14e01d6455468bb50d7a1
Hector Lopes <hector.lopes@anchieta.br>
2022-12-08 12:31:00 -0300

    ci: create json with hardware information and change mode of use map

--- src/Pages/Montar_pc/index.js
+++ src/Pages/Montar_pc/index.js
@@ -1,3 +1,9 @@
+fetch('./pecas.json')
+.then((response) => response.json())
+.then((vlores) => {
+
+console.log(vlores);
+
 function change_image() {
   const processador = $("#formu").val();
 
@@ -26,79 +32,60 @@ function change_image() {
 // document.getElementById("enviar").onclick = function (){
 //      $(".PROCESSADOR_DIV").hide();
 //  }
-const vlores = [
-  {
-    nomePeca: "Pentium",
-    preco: "",
-    quantidade: "",
-  },
-  {
-    nomePeca: "I3",
-    preco: "",
-    quantidade: "",
-  },
-  {
-    nomePeca: "I5",
-    preco: "",
-    quantidade: "",
-  },
-  {
-    nomePeca: "I7",
-    preco: "",
-    quantidade: "",
-  },
-  {
-    nomePeca: "I9",
-    preco: "",
-    quantidade: "",
-  },
-  {
-    nomePeca: "Ryzen 3",
-    preco: "",
-    quantidade: "",
-  },
-  {
-    nomePeca: "Ryzen 5",
-    preco: "",
-    quantidade: "",
-  },
-  {
-    nomePeca: "Ryzen 7",
-    preco: "",
-    quantidade: "",
-  },
-  {
-    nomePeca: "Ryzen 9",
-    preco: "",
-    quantidade: "",
-  },
-];
+
 
 function trocaformularios() {
-  //array objstos
-
-  var Chooseform = vlores.map(function (a, b) {
-    return ` <form >
-    <label  for="processador">Escolha o Processador</label>
-    <select onchange="change_image()" name="processador" id="processador">
-      <optgroup label="Intel">
-        <option value="${vlores[0].nomePeca}">${vlores[0].nomePeca}</option>
-        <option value="${vlores[1].nomePeca}">${vlores[1].nomePeca}</option>
-        <option value="${vlores[2].nomePeca}">${vlores[2].nomePeca}</option>
-        <option value="${vlores[3].nomePeca}">${vlores[3].nomePeca}</option>
-        <option value="${vlores[4].nomePeca}">${vlores[4].nomePeca}</option>
-      </optgroup>
-      <optgroup label="AMD">
-        <option value=${vlores[5].nomePeca}>${vlores[5].nomePeca}</option>
-        <option value=${vlores[6].nomePeca}>${vlores[6].nomePeca}</option>
-        <option value=${vlores[7].nomePeca}>${vlores[7].nomePeca}</option>
-        <option value=${vlores[8].nomePeca}>${vlores[8].nomePeca}</option>
+  //array objetos
   
-      </optgroup>
-    </select>`;
-  });
+  
 
-  document.querySelector("#teste2").innerHTML = Chooseform.join("");
 
-  console.log(Chooseform);
+
+//   let Chooseform = vlores.map(function () {
+//   //  if(index<)
+
+// // console.log(index)
+// // console.log(array)
+    
+
+//     return ` 
+//     <form >
+//     <label  for="processador">Escolha o Processador</label>
+//       <select onchange="change_image()" name="processador" id="processador">
+//         <optgroup label="Intel">
+//           <option value="${vlores[0].nomePeca}">${vlores[0].nomePeca}</option>
+//           <option value="${vlores[1].nomePeca}">${vlores[1].nomePeca}</option>
+//           <option value="${vlores[2].nomePeca}">${vlores[2].nomePeca}</option>
+//           <option value="${vlores[3].nomePeca}">${vlores[3].nomePeca}</option>
+//           <option value="${vlores[4].nomePeca}">${vlores[4].nomePeca}</option>
+//         </optgroup>
+//         <optgroup label="AMD">
+//           <option value=${vlores[5].nomePeca}>${vlores[5].nomePeca}</option>
+//           <option value=${vlores[6].nomePeca}>${vlores[6].nomePeca}</option>
+//           <option value=${vlores[7].nomePeca}>${vlores[7].nomePeca}</option>
+//           <option value=${vlores[8].nomePeca}>${vlores[8].nomePeca}</option>
+    
+//         </optgroup>
+//       </select>
+//     </form> `;
+  
+//   });
+
+
+  const teste = vlores.map((e) => {
+    const divId = document.querySelector("#teste2")
+
+    divId.innerHTML += `<option value="${e.nomePeca}">${e.nomePeca}</option>`
+  })
+
+  console.log(teste)
+
+  // document.querySelector("#teste2").innerHTML = Chooseform;
+
+  // console.log(Chooseform)
 }
+
+})
+.catch((err) => {
+  console.log(err);
+})
