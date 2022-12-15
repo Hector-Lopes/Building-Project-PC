f5a44009bd099729774b1b49c073b0e7a0e0ae3b
Hector Lopes <hector.lopes@anchieta.br>
2022-12-15 11:24:39 -0300

    feat:add the style of table and chance de filter of innerhtml

--- src/Pages/Montar_pc/index.js
+++ src/Pages/Montar_pc/index.js
@@ -9,48 +9,59 @@ fetch("./pecas.json")
   });
 
 function trocaformularios() {
-  const teste = $VLores.map((elemento, indice) => {
-    const divId = document.querySelector("#tabela");
-
-    divId.innerHTML += ` 
-      <div id="form">
-      <div id="foto">
-      <img id="img"   src="${elemento.imagem}" >
-      </div>  
-      <option value="${elemento.nomePeca}">${elemento.nomePeca}</option> 
-
-      </form> 
-     </div>
-      `;
-  });
-}
-
-function change_image() {
+ 
   const processador = $("#formu").val();
 
   switch (processador) {
     case "Processador":
-    
+      const teste = $VLores.map((elemento, indice) => {
+        const divId = document.querySelector("#tabela");
+       
+          divId.innerHTML += ` 
+            <div id="form">
+            <div id="foto">
+            <img id="img"   src="${elemento.imagem}" >
+            </div>  
+            <option value="${elemento.Tipo="Pentium"}">${elemento.nomePeca}</option> 
+      
+            </form> 
+           </div>
+            `;  
+          });
+          const Sidediv=document.querySelector(".section_2");
+          $(".pc").hide();
+          Sidediv.innerHTML +=` 
+          
+          <div class="side_menu">
+        
+          </div>
+          
+         `;
+       
       break;
     case "Placa Mãe":
-      trocaformularios();
-      $(".pc").hide();
+
       break;
     case "Placa de Video":
-      trocaformularios();
-      $(".pc").hide();
+     
       break;
     case "Memoria Ram":
-      trocaformularios();
-      $(".pc").hide();
+      
       break;
     case "SSHD":
-      trocaformularios();
-      $(".pc").hide();
+           
       break;
     default:
       console.log(`Sorry, we are out of ${expr}.`);
   }
+ 
+
+  
+   
 }
 
-//TIRAR A FUNÇÃO DO FECTH
+function change_image() {
+ 
+}
+
+
