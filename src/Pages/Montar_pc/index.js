6be489067b1f15e9d3f5dd318f98b53172b45b3f
Hector Lopes <hector.lopes@anchieta.br>
2022-12-21 10:54:21 -0300

    add: new hardware itens in Database

--- src/Pages/Montar_pc/index.js
+++ src/Pages/Montar_pc/index.js
@@ -10,29 +10,29 @@ fetch("./pecas.json")
 
 function trocaformularios() {
  
-  const processador = $("#formu").val();
+  const Droplist = $("#formu").val();
 
-  switch (processador) {
+  switch (Droplist) {
     case "Processador":
 
-    let Filtrar = $VLores.filter(function (Filtrado) {
+    let Filtrar_processador = $VLores.filter(function (Filtrado) {
       return Filtrado.Tipo === "Processador";
     })
     // .map(function (Filtrado) {
     //   return Filtrado.nomePeca;
       
     // })
-    console.log(Filtrar); 
-    const teste = Filtrar.map((elemento, indice) => {
+    console.log(Filtrar_processador); 
+    const PROCESSADOR = Filtrar_processador.map((elemento, indice) => {
          const divId = document.querySelector("#tabela");
 
 
           divId.innerHTML += ` 
             <div id="form">
-            <div id="foto">
+            <div id="foto_Processador">
             <img id="img"   src="${elemento.imagem}" >
             </div>  
-            <option value="${elemento.Tipo="Pentium"}">${elemento.nomePeca}</option> 
+            <option value="${elemento.Tipo}">${elemento.nomePeca}</option> 
       
             </form> 
            </div>
@@ -40,6 +40,9 @@ function trocaformularios() {
            });
           const Sidediv=document.querySelector(".section_2");
           $(".pc").hide();
+          $(".side_menu").hide();
+          
+         
           Sidediv.innerHTML +=` 
           
           <div class="side_menu">
@@ -50,6 +53,46 @@ function trocaformularios() {
        
       break;
     case "Placa Mãe":
+      let Filtrar_mae = $VLores.filter(function (Filtrado) {
+        return Filtrado.Tipo === "MAE";
+      })
+      // .map(function (Filtrado) {
+      //   return Filtrado.nomePeca;
+        
+      // })
+      console.log(Filtrar_mae); 
+      const MAE = Filtrar_mae.map((elemento, indice) => {
+           const divId = document.querySelector("#tabela");
+  
+  
+            divId.innerHTML += ` 
+              <div id="form">
+              <div id="foto_mae">
+              <img id="img"   src="${elemento.imagem}" >
+              </div>  
+              <option value="${elemento.Tipo}">${elemento.nomePeca}</option> 
+        
+              </form> 
+             </div>
+              `;  
+             });
+            const Sidediv_MAE=document.querySelector(".section_2");
+            $(".pc").hide();
+          $(".side_menu").hide();
+    
+         
+            Sidediv_MAE.innerHTML +=` 
+            
+            <div class="side_menu">
+          
+            </div>
+            
+           `;
+
+
+
+
+
 
       break;
     case "Placa de Video":
